892f076ffa7d71098cab3aaadcb36e6ece5e0073
Christofer Nolander <christofer.nolander@gmail.com>
2020-01-17 00:05:47 +0100

    Removed useless comment

--- src/lib.cpp
+++ src/lib.cpp
@@ -50,8 +50,6 @@ void Window::key_callback(GLFWwindow* glfw_window, int key, int scancode, int ac
   if (action == GLFW_PRESS) {
     keys.push_back(key);
   } else if (action == GLFW_RELEASE) {
-    // Find the released key and remove it by swapping it with the last element
-    // and then shrinking the vector. This is called a 
     for (int i = keys.size() - 1; i >= 0; i--) {
       if (keys[i] == key) {
         util::swap_remove(keys, i);

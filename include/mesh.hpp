22a3c99a8eaf02642519a06f85dc0b1f35251bb3
Christofer Nolander <christofer.nolander@gmail.com>
2020-02-13 11:38:54 +0100

    Document (most) classes and methods

--- include/mesh.hpp
+++ include/mesh.hpp
@@ -4,35 +4,63 @@
 
 #include <memory>
 
+/// The representation of a vertex.
 struct Vertex {
+  /// The position of the vertex in 3D space.
   float position[3];
+  /// The color of the vertex in RGBA format (normalized in the range 0-1).
   float color[4];
+  /// Coordinate to sample the bound texture from (normalized in the range 0-1).
   float texture_coord[2];
 };
 
+/// A handle to a mesh (a list triangles and its vertices).
 class Mesh {
   private:
+    /// Construct an empty mesh.
     Mesh();
 
+    /// Handle to the OpenGL Vertex Array Object (VAO). 
+    /// Automatically cleans up any resources upon destruction.
     std::shared_ptr<GLuint> vertex_array;
+
+    /// Handle to the OpenGL Vertex Buffer Object (VBO).
+    /// Automatically cleans up any resources upon destruction.
     std::shared_ptr<GLuint> vertex_buffer;
+
+    /// Handle to the OpenGL Element Buffer Object.
+    /// Automatically cleans up any resources upon destruction.
     std::shared_ptr<GLuint> element_buffer;
 
+    /// The number of vertices in the mesh.
     std::shared_ptr<GLuint> vertex_count;
+
+    /// The number of indices in the mesh.
     std::shared_ptr<GLuint> index_count;
 
+    // TODO: move this to its own class and make sure that the texture is
+    // deallocated when all handles are destroyed.
+    /// The texture associated with the mesh.
     GLuint texture;
 
   public:
+    /// Create a new, empty, mesh.
     static Mesh create();
 
+    /// Upload a list of vertices to the mesh.
     void set_vertices(GLuint count, Vertex* vertices);
+
+    /// Upload a list of indices to the mesh.
     void set_indices(GLuint count, GLuint* indices);
 
+    // TODO: move to its own class.
+    /// Load a texture from the specified path.
     void load_texture(const char* path);
 
+    /// Draw the mesh.
     void draw();
 
   private:
+    /// Setup the vertex and element buffers.
     void configure_buffers();
 };
--- include/shader.hpp
+++ include/shader.hpp
@@ -4,15 +4,24 @@
 
 #include "gl.h"
 
+/// A shader program (the combination of a vertex and fragment shader).
 class Program {
   private:
+    /// Create a new program from a raw OpenGL program handle.
     Program(GLuint program);
 
+    /// A handle to an OpenGL program handle.
+    /// Automatically deallocates the resource upon destruction.
     std::shared_ptr<GLuint> program;
 
   public:
+    /// Compile a vertex and fragment shader.
+    /// @param vertex_source The source code for a vertex.
+    /// @param fragment_source The source code for a fragment shader.
+    /// @returns An OpenGL shader program.
     static Program compile(const char* vertex_source, const char* fragment_source);
 
+    /// Bind this program to the OpenGL context. Unbinds any previous shaders.
     void use();
 };
 

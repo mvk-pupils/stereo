aa9f05a609e343541920f2c26e4053ba6e2913c9
Christofferos <42782387+Christofferos@users.noreply.github.com>
2020-05-13 16:28:48 +0200

    Doxygen comments added to main.cpp

--- example/src/window.cpp
+++ example/src/window.cpp
@@ -2,13 +2,16 @@
 #include <util.hpp>
 #include <algorithm>
 
-Window::Window() {
-	this->handle = nullptr;
-	this->state = std::make_shared<WindowState>(WindowState());
+Window::Window()
+{
+  this->handle = nullptr;
+  this->state = std::make_shared<WindowState>(WindowState());
 }
 
-Window Window::open(int width, int height) {
-  if (!glfwInit()) {
+Window Window::open(int width, int height)
+{
+  if (!glfwInit())
+  {
     throw 1;
   }
 
@@ -30,56 +33,70 @@ Window Window::open(int width, int height) {
   return window;
 }
 
-void Window::make_context_current() {
+void Window::make_context_current()
+{
   glfwMakeContextCurrent(this->handle);
 }
 
-bool Window::is_open() {
+bool Window::is_open()
+{
   return this->handle && !glfwWindowShouldClose(this->handle);
 }
 
-void Window::close() {
+void Window::close()
+{
   glfwDestroyWindow(this->handle);
   this->handle = nullptr;
 }
 
-void Window::get_inner_size(int* width, int* height) {
+void Window::get_inner_size(int *width, int *height)
+{
   glfwGetFramebufferSize(this->handle, width, height);
 }
 
-void Window::poll_events() {
+void Window::poll_events()
+{
   glfwPollEvents();
 }
 
-void Window::swap_buffers() {
+void Window::swap_buffers()
+{
   glfwSwapBuffers(this->handle);
 }
 
-bool Window::is_key_down(int key) {
+bool Window::is_key_down(int key)
+{
   auto start = this->state->pressed_keys.begin();
   auto end = this->state->pressed_keys.end();
   return std::find(start, end, key) != end;
 }
 
-void Window::setup_window_callbacks() {
+void Window::setup_window_callbacks()
+{
   // Store a pointer to the current `Window` class in the handle so that we can
   // reference it from the callbacks.
   glfwSetWindowUserPointer(this->handle, this->state.get());
   glfwSetKeyCallback(this->handle, Window::key_callback);
 }
 
-void Window::key_callback(GLFWwindow* glfw_window, int key, int scancode, int action, int mode) {
+void Window::key_callback(GLFWwindow *glfw_window, int key, int scancode, int action, int mode)
+{
   UNUSED(scancode);
   UNUSED(mode);
 
-  auto state = reinterpret_cast<WindowState*>(glfwGetWindowUserPointer(glfw_window));
-  auto& keys = state->pressed_keys;
+  auto state = reinterpret_cast<WindowState *>(glfwGetWindowUserPointer(glfw_window));
+  auto &keys = state->pressed_keys;
 
-  if (action == GLFW_PRESS) {
+  if (action == GLFW_PRESS)
+  {
     keys.push_back(key);
-  } else if (action == GLFW_RELEASE) {
-    for (size_t i = 0; i < keys.size(); i++) {
-      if (keys[i] == key) {
+  }
+  else if (action == GLFW_RELEASE)
+  {
+    for (size_t i = 0; i < keys.size(); i++)
+    {
+      if (keys[i] == key)
+      {
         util::swap_remove(keys, i);
         i--;
       }
--- include/videodecoder.hpp
+++ include/videodecoder.hpp
@@ -1,27 +1,27 @@
 #include "gl.h"
 
-struct Frame {
+struct Frame
+{
 
   GLuint texture = 0; //OpenGL texture
-  int number; //frame index
+  int number;         //frame index
   int width;
   int height;
-
 };
 
-enum class Playback {
-    PLAY, PAUSE, FFW
+enum class Playback
+{
+  PLAY,
+  PAUSE,
+  FFW
 };
 
-class VideoDecoder{
+class VideoDecoder
+{
 public:
-
   virtual Frame next_frame() = 0;
 
   virtual void set_playback(Playback) = 0;
 
-  virtual int total_frames() = 0;
-
   virtual double frame_rate() = 0;
-
 };
